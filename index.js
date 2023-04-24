98e612daf4255d430b12e08a1a7e9857d02af6da
Victory Achuba <117039679+VICTORYACH@users.noreply.github.com>
2023-04-24 16:07:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Import Classes Here
+class Product{
+    constructor(name,price,description){
+        this.name=name
+        this.price=price
+        this.description=description
 
-
-
-
-
-
-
+    }
+    instock(){
+        return true
+    }
+    display(){
+        return "Name:"+this.name+", Price: $"+this.price+", Description:"+this.description
+    }
+}
 
 
 
